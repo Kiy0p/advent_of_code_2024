80d5c858786e3ccc7ee9b51598166f74aeea6949
Enzo Doguereau <edoguereau@yahoo.fr>
2024-12-09 23:01:34 -0500

    day01

---
+++ README.md
@@ -0,0 +1,15 @@
+# My code for the advent of code 2024 event.
+I decided to write it in kotlin.
+
+## Prerequisites :
+You need to install the kotlin language compiler :
+With apt : sudo apt  install kotlin
+With SDKMan : sdk install kotlin
+With Homebrew : brew update; brew install kotlin
+With snap : sudo snap install --classic kotlin
+
+## How to compile :
+kotlinc [day01.kt] -include-runtime -d [day01.jar]
+
+## How to run :
+java -jar [day01.jar]
---
+++ day01/day01.kt
@@ -0,0 +1,28 @@
+import java.io.File
+import kotlin.math.abs
+
+fun main() {
+    /* PART 1 */
+    val filePath = "./resources/input.txt"
+    val lists = Pair(mutableListOf<Int>(), mutableListOf<Int>())
+
+    File(filePath).forEachLine {
+        val lineList = it.split("   ").map{it.toInt()}
+        lists.first.add(lineList[0]) 
+        lists.second.add(lineList[1])
+    }
+
+    lists.first.sortDescending()
+    lists.second.sortDescending()
+
+    var result1 = 0
+    lists.first.forEachIndexed{ index, number -> result1 += abs(number - lists.second[index]) }
+
+    println("part 1 : " + result1)
+
+    /* PART 2 */
+
+    var result2 = lists.first.fold(0){ acc, number -> acc + number * lists.second.count{it == number} }
+
+    println("part 2 : " + result2)
+}
